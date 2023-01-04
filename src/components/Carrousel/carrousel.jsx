292a29532ef34622edc45de3aa222fe514b50959
Elsa-B <elsaz2.barrero@gmail.com>
2023-01-04 14:58:27 +0100

    Police

---
+++ src/components/Carrousel/carrousel.jsx
@@ -0,0 +1,15 @@
+//import Data from "../../data/data.json"
+
+function Carrousel(){
+    return
+        /*<div>
+		{Data.map((slide) => (
+			<div key={slide.id} >
+				<img src={slide.pictures} alt="logement" />
+			</div>
+		))}
+        </div>*/
+    
+}
+
+export default Carrousel;
--- src/pages/Accomodation/accomodation.jsx
+++ src/pages/Accomodation/accomodation.jsx
@@ -1,10 +1,12 @@
 import Header from "../../components/Header/header";
 import Footer from "../../components/Footer/footer";
+import Carrousel from "../../components/Carrousel/carrousel" 
 
 function Accomodation(){
     return(
         <>
         <Header/>
+        <Carrousel/>
         <Footer/>
         </>
     )
