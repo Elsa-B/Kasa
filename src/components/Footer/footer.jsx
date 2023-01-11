b92553e41d67b02f18b94a39c010219f9cdd423d
Elsa-B <elsaz2.barrero@gmail.com>
2023-01-11 15:34:35 +0100

    CSS footer/header

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,15 +1,14 @@
 import { Link } from "react-router-dom";
 import LogoWhite from '../../assets/logowhite.png';
-import FooterCss from '../Footer/footer.module.css'
 
 function Footer(){
     return(
-        <footer className={FooterCss.footer}>
-            <div className={FooterCss.div}>
+        <footer>
+            <div>
                 <Link to="/">
-                <img src={LogoWhite} alt= "Logo KASA" className={FooterCss.img}/> 
+                <img src={LogoWhite} alt= "Logo KASA"/> 
                 </Link>
-                <p className={FooterCss.p}>© 2020 Kasa. All rights reserved</p>
+                <p>© 2020 Kasa. All rights reserved</p>
             </div>    
         </footer>
     )
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,18 +1,17 @@
 import { Link } from "react-router-dom";
 import Logo from '../../assets/logo.png';
-import Headercss from '../Header/header.module.css'
 
 function Header(){
     return(
-        <header className={Headercss.header}>
+        <header>
             <div>
                 <Link to="/">
-                  <img src={Logo} alt= "Logo KASA" className={Headercss.img}/> 
+                  <img src={Logo} alt= "Logo KASA"/> 
                 </Link>
             </div>
-            <nav className={Headercss.nav}>
-                <Link className={Headercss.linkNav} to="/">Accueil</Link>
-                <Link className={Headercss.linkNav} to="/about">A Propos</Link>
+            <nav>
+                <Link className="linkNav" to="/">Accueil</Link>
+                <Link className="linkNav" to="/about">A Propos</Link>
             </nav>
         </header>
     )
