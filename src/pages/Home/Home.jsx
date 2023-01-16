fc843f6557dc1ec9c9383315825de3c82cda30c7
Elsa-B <elsaz2.barrero@gmail.com>
2023-01-16 14:48:30 +0100

    Finalisation projet

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import Banner from "../../components/Banner/Banner"
+import Banner from "../../components/Banner/banner"
 import Card from "../../components/Card/card"
 import { Link } from "react-router-dom"
 import Data from "../../data/data.json"
