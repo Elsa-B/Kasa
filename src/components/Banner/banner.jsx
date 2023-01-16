31b6ec242c6ec64b0e00613d40c72a3e2444faba
Elsa-B <elsaz2.barrero@gmail.com>
2023-01-16 14:47:13 +0100

    changement nom

--- src/components/Banner/banner.jsx
+++ src/components/Banner/banner.jsx
@@ -1,4 +1,3 @@
-
 function Banner({img, imgMobile=null, text}){
     return(
         <section className="bannersection">
@@ -10,15 +9,5 @@ function Banner({img, imgMobile=null, text}){
         </section>
     )
 }
-
-/*export default BannerAbout;
-function Banner(){
-    return(
-        <section className="bannerdiv">
-            <img src={imgBanner} alt= "Vue sur un paysage" className="bannerimg"/>
-            <h1 className="bannerh1">Chez vous, partout et ailleurs</h1>
-        </section>
-    )
-}*/
 
 export default Banner;
